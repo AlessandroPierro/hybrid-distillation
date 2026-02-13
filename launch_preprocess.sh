77e8d661bdc774dbb9b45422c6d6e38f0dedd799
Alessandro Pierro <40338642+AlessandroPierro@users.noreply.github.com>
2026-02-13 05:17:55 -0800

    Update download script
    Signed-off-by: Alessandro Pierro <alessandro.pierro@intel.com>

--- launch_preprocess.sh
+++ launch_preprocess.sh
@@ -16,25 +16,25 @@ mkdir -p logs
 # Load environment or activate venv if necessary
 # source .venv/bin/activate
 
-uv run python preprocess_download_tokenize.py \
-  --dataset_name HuggingFaceFW/fineweb-edu \
-  --dataset_config sample-10BT \
-  --split train \
-  --text_field text \
-  --tokenizer fla-hub/Qwen2.5-3B-Instruct \
-  --output_dir data_cache/tokenized_dataset \
-  --num_proc 96
+#HF_HOME="/export/work/apierro/" python preprocess_download_tokenize.py \
+#  --dataset_name HuggingFaceFW/fineweb-edu \
+#  --dataset_config sample-10BT \
+#  --split train \
+#  --text_field text \
+#  --tokenizer fla-hub/Qwen2.5-3B-Instruct \
+#  --output_dir /export/work/apierro/data_cache/tokenized_dataset \
+#  --num_proc 64
 
 # Chunking for Stage 1 (Short Context)
-uv run python preprocess_chunk.py \
-  --tokenized_dataset_path data_cache/tokenized_dataset \
+HF_HOME="/export/work/apierro/" python preprocess_chunk.py \
+  --tokenized_dataset_path /export/work/apierro/data_cache/tokenized_dataset \
   --context_length 512 \
-  --output_dir data_cache \
-  --npy_cache_path data_cache/tokenized_5pct_512.npy
+  --output_dir /export/work/apierro/data_cache \
+  --npy_cache_path /export/work/apierro/data_cache/tokenized_5pct_512.npy
 
 # Chunking for Stage 2 (Long Context)
-uv run python preprocess_chunk.py \
-  --tokenized_dataset_path data_cache/tokenized_dataset \
+HF_HOME="/export/work/apierro/" python preprocess_chunk.py \
+  --tokenized_dataset_path /export/work/apierro/data_cache/tokenized_dataset \
   --context_length 4096 \
-  --output_dir data_cache \
-  --npy_cache_path data_cache/tokenized_5pct_4096.npy
+  --output_dir  /export/work/apierro/data_cache \
+  --npy_cache_path  /export/work/apierro/data_cache/tokenized_5pct_4096.npy
